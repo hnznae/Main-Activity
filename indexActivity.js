8b9b25e5b74b152fa9da785b241158c8fdae12e2
Hanzen Nikolai R. Munoz <hanzen.munoz@gmail.com>
2023-03-21 09:39:43 +0800

    Add Activity Code

---
+++ indexActivity.js
@@ -0,0 +1,19 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let firstName = "Hanzen Nikolai";
+let lastName = "Munoz";
+let age = 19;
+let hobbies = ["Poetry", "Video Games", "Editing", "Scriptwriting"];
+
+let homeAddress = {
+	houseNumber: 6,
+	street: 'Sepo St.',
+	subd: 'Karlaville',
+	barangay: 'Wawa II',
+	city: 'Rosario',
+	province: 'Cavite',
+	zipC: 4106
+};
+
+console.log("I am " + `${firstName} ${lastName}` + " I'm currently " + `${age}` + " and I like " + `${hobbies}` + " ...I live in ");
+console.log(homeAddress);
